23c086ab114133ab137c1a7d50e10a337a819958
serge_kamya <sergekamya@gmail.com>
2021-09-09 14:35:46 +0200

    displays hello world from a class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
-};
+ class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World from a class component</h1>
+      </div>
+    )
+  }
+}
 
-export default App;
+export default App
