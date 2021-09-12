01a2afa6cd834009764ba73436cb4d63df293f5b
serge_kamya <sergekamya@gmail.com>
2021-09-12 20:17:21 +0200

    added spec for GreetingOutput using Jest

--- src/App.jsx
+++ src/App.jsx
@@ -14,31 +14,3 @@ const App = () => {
 };
 
 export default App;
-
-// import React, { Component } from "react";
-// import GreetingInput from './GreetingInput'
-// import GreetingOutput from './GreetingOutput'
-
-// export class App extends Component {
-//   state = {
-//     planet: "",
-//   };
-
-//   textInput(event) {
-//     this.setState({ planet: event.target.value });
-//   }
-
-//   render() {
-//     //const { planet } = this.state;
-
-//     return (
-//       <>
-//         <GreetingOutput planet= {this.state.planet}/>
-//         <GreetingInput textInput={this.textInput.bind(this)} />
-
-//       </>
-//     );
-//   }
-// }
-
-// export default App;
--- src/GreetingOutput.jsx
+++ src/GreetingOutput.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const GreetingOutput = (props) => {
   return (
     <div>
-      <h1>Hello {props.planet} from a class component</h1>
+      <h1 id="greeting">Hello {props.planet.name} from a functional component</h1>
     </div>
   );
 };
